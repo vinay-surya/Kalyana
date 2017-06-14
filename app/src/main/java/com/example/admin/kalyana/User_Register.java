ab7f05b3e035bbfab49721addb529e691c08b510
vinay surya <vinisuri@gmail.com>
2017-06-14 14:42:34 +0530

    initial commit.

---
+++ app/src/main/java/com/example/admin/kalyana/User_Register.java
@@ -0,0 +1,17 @@
+package com.example.admin.kalyana;
+
+
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class User_Register extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user__register);
+
+
+    }
+}
